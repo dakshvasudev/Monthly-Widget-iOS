6a77f1ee245bf000168d3ed071c1ac0e900cc2b5
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2024-06-03 14:48:50 +0530

    Initial Commit

---
+++ monthly/monthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  monthlyApp.swift
+//  monthly
+//
+//  Created by daksh vasudev on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct monthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
